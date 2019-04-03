6ed12d9ac6858d188458779a893542e9a2c17c4b
NorbertLam <noobertlm@gmail.com>
2019-04-03 10:38:07 -0400

    add streamer resources

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :username, :password, :first_name, :profile_picture)
+  end
+
 end
