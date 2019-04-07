9d084731c501f189d404668125b049d935c9c439
NorbertLam <noobertlm@gmail.com>
2019-04-07 11:29:50 -0400

    add downcase for email

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
  
   def create
-    @user = User.find_by(email: user_login_params[:email])
+    @user = User.find_by(email: user_login_params[:email].downcase)
 
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: @user.id })
